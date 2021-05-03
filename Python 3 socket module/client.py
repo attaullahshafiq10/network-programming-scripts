bfd1667cf9c708498493257cc9c62af2f523c7cd
Hafiz Muhammad Attaullah <attaullahshafiq10@gmail.com>
2021-05-03 13:55:26 +0500

    Create client.py

---
+++ Python 3 socket module/client.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python           # This is client.py file
+# HM Atta
+
+import socket               # Import socket module
+
+s = socket.socket()         # Create a socket object
+host = socket.gethostname() # Get local machine name
+port = 12345                # Reserve a port for your service.
+
+s.connect((host, port))
+print s.recv(1024)
+s.close()                     # Close the socket when done
