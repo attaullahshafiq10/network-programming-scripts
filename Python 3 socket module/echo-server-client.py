2f80f6c9a6e3b47fadb3962eaa5ec76f44a9b47c
Hafiz Muhammad Attaullah <attaullahshafiq10@gmail.com>
2021-05-03 13:44:36 +0500

    Update echo-server-client.py

--- Python 3 socket module/echo-server-client.py
+++ Python 3 socket module/echo-server-client.py
@@ -8,7 +8,7 @@ HOST = 'localhost'
 PORT = 50007
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
-s.send(b 'Hello, Attaullah')
+s.send(b'Hello, Attaullah')
 data = s.recv(1024)
 s.close()
 print('Recieved' , repr(data))
