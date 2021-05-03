2be6788733fbbd4dbbbf4dfd990fc1481d7119a9
Hafiz Muhammad Attaullah <attaullahshafiq10@gmail.com>
2021-05-03 13:19:48 +0500

    Create echo-server.py

---
+++ Python 3 socket module/echo-server.py
@@ -0,0 +1,21 @@
+# Echo server Program
+# HM Attaullah
+# mr_attaullah@hotmail.com
+
+import socket
+
+HOST = socket.gethostname()
+PORT = 50007
+s = socket.socket(socket.AF_INF, socket.SOCK(STREAM)
+s.bind((HOST, PORT))
+s.listen(1)
+conn, addr = s.accept()
+print("connected by" , addr)
+while True:
+    data = conn.recv(1024)
+    if not data: break
+    conn.send(data)
+conn.close()                  
+                  
+                  
+                  
